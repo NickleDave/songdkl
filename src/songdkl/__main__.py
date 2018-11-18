22cde7ebf32041a8774b0cb32df9facdc1f36ec6
nickledave <nicholdav@gmail.com>
2018-11-18 11:22:56 -0500

    revamp src/songdkl/__main__.py

--- src/songdkl/__main__.py
+++ src/songdkl/__main__.py
@@ -5,27 +5,35 @@ The same function is run by the script `songdkl-cli` which is installed on the
 path by pip, so `$ songdkl-cli --help` would have the same effect (i.e., no need
 to type the python -m)
 """
-
 import argparse
-import os
-from glob import glob
-
 import songdkl
 
-parser = argparse.ArgumentParser(description='main script',
-                                 formatter_class=argparse.RawTextHelpFormatter,)
-parser.add_argument('-c', '--compute', type=str,
-                    help='compute SongD(KL) metric between two birds'
-                    '.ini file, by passing the name of that file.\n'
-                            '$ songdkl-cli --compute ./bird1_songs/ ./bird2_songs/')
-parser.add_argument('-n', '--numsyls', type=str,
-                    help='use SongD(KL) metric to estimate number of '
-                         'syllables in one bird's song.\n'
-                         '$ cnn-bilstm --glob ./config_finches*.ini')
-args = parser.parse_args()
+def parse_compute_args(string):
+    """"convert arg from str to int, if it doesn't raise an error.
+    gets applied to every arg for --compute"""
+    try:
+        string = int(string)
+        return string
+    except ValueError:
+        return string
+
+
+def parse_songdkl_args():
+    parser = argparse.ArgumentParser(description='main script',
+                                     formatter_class=argparse.RawTextHelpFormatter,)
+    parser.add_argument('-c', '--compute', nargs=4, type=parse_compute_args,
+                        help='compute SongD(KL) metric between two birds'
+                             '.ini file, by passing the name of that file.\n'
+                             '$ songdkl-cli --compute ./bird1_songs/ ./bird2_songs/')
+    parser.add_argument('-n', '--numsyls', type=str,
+                        help='use SongD(KL) metric to estimate number of '
+                             'syllables in one bird\'s song.\n'
+                             '$ songdkl-cli --numsyls ./bird1_songs/')
+    return parser.parse_args()
 
 
 def main():
+    args = parse_songdkl_args()
     if sum([bool(arg)
             for arg in [args.compute, args.numsyls,]
             ]) != 1:
@@ -33,12 +41,13 @@ def main():
                      "--compute, --numsyls.\n"
                      "Run 'songdkl-cli --help' for an explanation of each.")
 
-    if args.compuyte:
-        config_files = glob(args.glob)
+    if args.compute:
+        songdkl.compute_songdkl(path1=args.compute[0],
+                                path2=args.compute[1],
+                                k=args.compute[2],
+                                k2=args.compute[3])
     elif args.numsyls:
-        with open(args.txt, 'r') as config_list_file:
-            config_files = config_list_file.readlines()
-
+        songdkl.numsyls()
 
 if __name__ == "__main__":
     main()
