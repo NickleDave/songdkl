caa74968bdad8b24c9330aa0caebcdb1e2542f0c
NickleDave <nicholdav@gmail.com>
2019-11-03 15:30:46 -0500

    rewrite songdkl/__main__.py to import parser and use it

--- src/songdkl/__main__.py
+++ src/songdkl/__main__.py
@@ -5,49 +5,24 @@ The same function is run by the script `songdkl-cli` which is installed on the
 path by pip, so `$ songdkl-cli --help` would have the same effect (i.e., no need
 to type the python -m)
 """
-import argparse
-import songdkl
-
-def parse_compute_args(string):
-    """"convert arg from str to int, if it doesn't raise an error.
-    gets applied to every arg for --compute"""
-    try:
-        string = int(string)
-        return string
-    except ValueError:
-        return string
-
-
-def parse_songdkl_args():
-    parser = argparse.ArgumentParser(description='main script',
-                                     formatter_class=argparse.RawTextHelpFormatter,)
-    parser.add_argument('-c', '--compute', nargs=4, type=parse_compute_args,
-                        help='compute SongD(KL) metric between two birds'
-                             '.ini file, by passing the name of that file.\n'
-                             '$ songdkl-cli --compute ./bird1_songs/ ./bird2_songs/')
-    parser.add_argument('-n', '--numsyls', type=str,
-                        help='use SongD(KL) metric to estimate number of '
-                             'syllables in one bird\'s song.\n'
-                             '$ songdkl-cli --numsyls ./bird1_songs/')
-    return parser.parse_args()
+from .parser import get_parser
+from .songdkl import compute_songdkl
+from .numsyls import numsyls
 
 
 def main():
-    args = parse_songdkl_args()
-    if sum([bool(arg)
-            for arg in [args.compute, args.numsyls,]
-            ]) != 1:
-        parser.error("Please specify exactly one of the following flags: "
-                     "--compute, --numsyls.\n"
-                     "Run 'songdkl-cli --help' for an explanation of each.")
+    parser = get_parser()
+    args = parser.parse_args()
 
     if args.compute:
-        songdkl.compute_songdkl(path1=args.compute[0],
-                                path2=args.compute[1],
-                                k=args.compute[2],
-                                k2=args.compute[3])
+        compute_songdkl(path1=args.compute.path1,
+                        path2=args.compute.path2,
+                        k=args.compute.nsyls1,
+                        k2=args.compute.nsyls2)
+
     elif args.numsyls:
-        songdkl.numsyls()
+        numsyls()
+
 
 if __name__ == "__main__":
     main()
