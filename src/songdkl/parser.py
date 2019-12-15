a3c1279e2fac904852a71c7acd30692fe509e47f
David Nicholson <nickledave@users.noreply.github.com>
2019-12-15 06:43:52 -0500

    rewrite help for calculate sub-parser

--- src/songdkl/parser.py
+++ src/songdkl/parser.py
@@ -1,31 +1,34 @@
 import argparse
 
-COMPUTE_HELP = """
+CALCULATE_HELP = """
 calculate the song divergence between two birds. It is applied as follows:
 
-$ songdkl compute folder_with_songs_from_bird_1 folder_with_songs_from_bird_2 number_of_syllable_calsses_bird_1 number_of_syllable_classes_bird_2
+$ songdkl calculate path1 path2 n_syl1 n_syl2
 
-e.g.,  
+where:
+    path1 : string, folder with songs from bird 1
+    path2 : string, folder with songs from bird 2
+    n_syl1 : int, number of syllable_classes for bird 1
+    n_syl2 : int, number of syllable_classes for bird 2
 
-$ songdkl compute bird_data/y25/ bird_data/y34br6/ 9 10
+e.g.,
+
+$ songdkl calculate ~/data/bird_data/y25/ ~/data/bird_data/y34br6/ 9 10
 
 It expects the songs to be in mono wave format and have a .wav suffix.
 The output is a tab delimited string formatted as follows:
 
-foldername_bird1 foldername_bird2 n_syl_classes_bd1 n_syl_classes_bd2 n_basis_set SD_bd1_ref_bd2_song SD_bd2_ref_bd1_comp n_syls_bd1 n_syls_bd2
+directory1 directory2 n_syl1 n_syl2 n_basis_set SD_bd1_ref_bd2_song SD_bd2_ref_bd1_comp n_syls_bd1 n_syls_bd2
 
 e.g. 
 
 y25 y32br6 9 10 50 0.039854682578 0.0340690226514 3000 3000
 
-IMPORTANT! In Mets Brainard 2018,  the number of syllables in the tutor song for both 
-syllable # values. This is meant to be conservative, basically give the bird learning the 
-benefit of the doubt that it actually coppied all of the syllables in the tutor song.  
-Empirically, changing these numbers doesn't have much impact on the divergence calculations 
-(see the paper)
-
-IMPORTANT! Throughout the paper we calculated PSDs for the raw wave forms of syllables. 
-This is the default setting for this script. If your song is contaminated with low 
+Notes
+-----
+Throughout the paper we calculated PSDs for the raw wave forms of syllables. 
+The default setting for this script. 
+If your song is contaminated with low 
 frequency noise this noise may be incorporated into the model for song potentially 
 causing over estimates of song D_KL if there is low frequency noise in the tutor song but 
 not the tutee song.  This could occur if the birds were recorded under different 
@@ -33,6 +36,12 @@ conditions or in different sound recording boxes.  If you uncomment line 99 belo
 the script will calculate the song D_KL using filtered syllable data. We only advise 
 this if you have low frequency noise that is differential between the tutor and tutee 
 song and you don't want that noise incorporated into the song D_KL calculations.
+
+In Mets Brainard 2018,  the number of syllables in the tutor song for both 
+syllable # values. This is meant to be conservative, basically give the bird learning the 
+benefit of the doubt that it actually copied all of the syllables in the tutor song.  
+Empirically, changing these numbers doesn't have much impact on the divergence calculations 
+(see the paper)
 """
 
 NUMSYLS_HELP = """
