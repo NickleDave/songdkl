21fb1fb4447d4d39c8d216e778b081216bef6007
David Nicholson <nickledave@users.noreply.github.com>
2022-10-13 22:19:33 -0400

    Fix where bash scripts look for PCB dataset [skip ci]

--- tests/scripts/calc-sylno-pcb.sh
+++ tests/scripts/calc-sylno-pcb.sh
@@ -5,7 +5,7 @@ PCB_SCRIPT_SYL_COUNTS_FILE=tests/data-for-tests/generated/pcb-script-syl-counts.
 rm -f $PCB_SCRIPT_SYL_COUNTS_FILE
 touch $PCB_SCRIPT_SYL_COUNTS_FILE
 
-BIRD_FOLDERS=(./tests/data-for-tests/song_data/*/)
+BIRD_FOLDERS=(./data/pcb_data/song_data/*/)
 for BIRD_FOLDER in ${BIRD_FOLDERS[@]:1:$FIRST_N};do
   echo running script for $BIRD_FOLDER
   conda run -n songdkl-pcb python ./src/pcb-scripts/calc_sylno.py $BIRD_FOLDER >> $PCB_SCRIPT_SYL_COUNTS_FILE
--- tests/scripts/calc-sylno-py-pkg.sh
+++ tests/scripts/calc-sylno-py-pkg.sh
@@ -6,7 +6,7 @@ PY_PKG_SYL_COUNTS_FILE=tests/data-for-tests/generated/py-pkg-syl-counts.txt
 rm -f $PY_PKG_SYL_COUNTS_FILE
 touch $PY_PKG_SYL_COUNTS_FILE
 
-BIRD_FOLDERS=(./tests/data-for-tests/song_data/*/)
+BIRD_FOLDERS=(./data/pcb_data/song_data/*/)
 for BIRD_FOLDER in ${BIRD_FOLDERS[@]:1:$FIRST_N};do
   echo running songdkl for $BIRD_FOLDER
   songdkl numsyls $BIRD_FOLDER >> $PY_PKG_SYL_COUNTS_FILE
