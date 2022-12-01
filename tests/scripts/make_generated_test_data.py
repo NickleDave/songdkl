abbdaa0b7d6e23fc9fefbdd02ee0f61f7c542ef1
David Nicholson <nickledave@users.noreply.github.com>
2022-11-30 22:06:52 -0500

    Add tests/scripts/make_generated_test_data.py

---
+++ tests/scripts/make_generated_test_data.py
@@ -0,0 +1,63 @@
+"""Make the 'generated' test data,
+i.e., make outputs using ``songdkl``
+from inputs that would be the source/raw
+data files.
+This lets us run tests without re-creating
+generated data for each unit test.
+"""
+import pathlib
+import shutil
+
+import songdkl
+
+
+TEST_DATA_ROOT = pathlib.Path(__file__).parent / '..' / 'data-for-tests'
+SOURCE_DATA_ROOT = TEST_DATA_ROOT / 'source'
+SONG_DATA_ROOT = SOURCE_DATA_ROOT / 'song_data'
+SONG_DATA_SUBDIRS = [
+    dir_ for dir_ in SONG_DATA_ROOT.iterdir() if dir_.is_dir()
+]
+
+GENERATED_DATA_ROOT = TEST_DATA_ROOT / 'generated'
+GENERATED_SONG_DATA_ROOT = GENERATED_DATA_ROOT / 'song_data'
+
+
+def prep_and_save():
+    """Prepare arrays as done by ``songdkl.prep.prep`` function,
+    and save as .zarr files in compressed format,
+    to use in tests."""
+    for song_data_subdir in SONG_DATA_SUBDIRS:
+        print(
+            f"Preparing data from: {song_data_subdir}"
+        )
+        output_dir_path = GENERATED_SONG_DATA_ROOT / song_data_subdir.name
+        output_dir_path.mkdir(exist_ok=True, parents=True)
+        songdkl.prep.prep_and_save(dir_path=song_data_subdir,
+                                   output_dir_path=output_dir_path)
+
+
+# --- main script ----
+print(
+    "Generating data for tests."
+)
+
+print(
+    "Preparing and saving arrays of PSDs from syllable segments."
+)
+prep_and_save()
+
+print(
+    f'making archive from {GENERATED_SONG_DATA_ROOT}'
+)
+shutil.make_archive(
+    './tests/data-for-tests/generated/generated-test-data',
+    'gztar',
+    root_dir=GENERATED_DATA_ROOT,
+    # specify `base_dir` since we only want to archive song_data
+    base_dir="song_data",  # needs to be written relative to root
+)
+
+
+print(
+    "Done preparing generated data for tests."
+)
