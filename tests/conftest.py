c554999b9c7d998df73822e99fdf430a437b859a
David Nicholson <nickledave@users.noreply.github.com>
2022-12-03 21:41:37 -0500

    Add kwargify fixtures to tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,14 @@
+import pytest
+
+
 from .fixtures import *
+
+
+@pytest.fixture
+def kwargify():
+    def _kwargify(**kwargs):
+        return {
+            k: v
+            for k, v in kwargs.items() if v is not None
+        }
+    return _kwargify
