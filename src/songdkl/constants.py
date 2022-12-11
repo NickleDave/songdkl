9342908e8c41878a4b16afaea53094c9fde7aea9
David Nicholson <nickledave@users.noreply.github.com>
2022-12-10 22:38:35 -0500

    Add DefaultGaussianMixtureKwargs to songdkl/constants.py

--- src/songdkl/constants.py
+++ src/songdkl/constants.py
@@ -1,2 +1,16 @@
+import dataclasses
+
+
 # format for timestamps
 STRFTIME_TIMESTAMP = "%y%m%d_%H%M%S"
+
+
+@dataclasses.dataclass
+class DefaultGaussianMixtureKwargs:
+    max_iter: int = 100000
+    n_init: int = 5
+    covariance_type: str = 'full'
+    random_state: int = 42
+
+
+DEFAULT_GMM_KWARGS = DefaultGaussianMixtureKwargs()
